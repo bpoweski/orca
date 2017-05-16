4a1a4110f0d27bb7b852cad421c6bd0c475a7975
Ben Poweski <bpoweski@gmail.com>
2017-05-16 18:20:14 -0500

    upgrade to orc-core 1.4.0

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.apache.orc/orc-core "1.3.3"]]
+                 [org.apache.orc/orc-core "1.4.0"]]
   :profiles {:dev {:resource-paths ["test-resources"]}})
